8f790917ecb22371ce7353aca4f46cf9a3a49860
erikn775 <erikn775@gmail.com>
2021-01-25 13:07:35 -0700

    commit tree d157158643be22d9938890011a0fde045449437c

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    var customerName = 'bob';
+    var newName = customerName.toUpperCase();
+    console.log(newName);
+}
+
+
+// function setBestCustomer() {
+
+// }
